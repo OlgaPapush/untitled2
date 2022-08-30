0b2a7403c520e3a43377a6377e4cf6904320465a
Ольга <koval.oi.1106@gmail.com>
2022-08-30 20:06:48 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)   {
+
+        // задание 1
+
+            for ( int i = 0 ; i < 10 ;  i ++) {
+        System.out.println ( "Итерация цикла " + i ) ;
+         }
+       int start = 10;
+            while ( start > 0) {
+                System.out.println ( start );
+                start -- ;
+            }
     }
 }
